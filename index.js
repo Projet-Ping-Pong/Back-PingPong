bf2594198f8db7011464b99e04589dfad6100846
jeremy courby <courby.jeremy@gmail.com>
2024-06-10 15:36:11 +0200

    env plateform.sh

--- index.js
+++ index.js
@@ -4,19 +4,28 @@ const app = express()
 
 app.use(express.json())
 
-const dsn = process.env.CONNECTION_STRING
+var dsn = process.env.CONNECTION_STRING
+if (dsn === undefined) {
+  const { env } = process;
+  const read_base64_json = function (varName) {
+    try {
+      return JSON.parse(Buffer.from(env[varName], "base64").toString())
+    } catch (err) {
+      throw new Error(`no ${varName} environment variable`)
+    }
+  };
+  const variables = read_base64_json('PLATFORM_VARIABLES')
+  dsn = variables["CONNECTION_STRING"]
+}
+
 console.log(`Using database ${dsn}`)
 const port = process.env.PORT || 3333;
 
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
-  })
-
-app.get('/test2', (req, res) => {
-  res.send('test')
+  res.send('Hello World!')
 })
 
 app.listen(port, () =>
- console.log(`Listening on the port ${port}`)
+  console.log(`Listening on the port ${port}`)
 )
