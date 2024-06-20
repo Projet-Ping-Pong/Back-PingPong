4bd4a76264710fb93c243f7464c0b6aa5ab03a8f
jeremy courby <courby.jeremy@gmail.com>
2024-06-20 14:53:20 +0200

    Commit pour plateform.sh

--- src/Security/auth.js
+++ src/Security/auth.js
@@ -2,7 +2,24 @@ const { sign, verify } = require('jsonwebtoken');
 const utiRepository = require('../models/utilisateur-repository');
 
 exports.generateAuthToken = (id_user, name_user) => {
-    return sign({ id_user, name_user }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXPIRES_IN });
+
+    var jwt_secret = process.env.JWT_SECRET
+    var jwt_expires_in = process.env.JWT_EXPIRES_IN
+    if (jwt_secret === undefined || jwt_expires_in === undefined) {
+        const { env } = process;
+        const read_base64_json = function (varName) {
+            try {
+                return JSON.parse(Buffer.from(env[varName], "base64").toString())
+            } catch (err) {
+                throw new Error(`no ${varName} environment variable`)
+            }
+        };
+        const variables = read_base64_json('PLATFORM_VARIABLES')
+        jwt_secret = variables["JWT_SECRET"]
+        jwt_expires_in = variables["JWT_EXPIRES_IN"]
+    }
+
+    return sign({ id_user, name_user }, jwt_secret, { expiresIn: jwt_expires_in });
 };
 
 exports.validateJWT = (req, res, next) => {
@@ -10,8 +27,25 @@ exports.validateJWT = (req, res, next) => {
         res.status(401).end()
         return
     }
+
+    var jwt_secret = process.env.JWT_SECRET
+    var jwt_expires_in = process.env.JWT_EXPIRES_IN
+    if (jwt_secret === undefined || jwt_expires_in === undefined) {
+        const { env } = process;
+        const read_base64_json = function (varName) {
+            try {
+                return JSON.parse(Buffer.from(env[varName], "base64").toString())
+            } catch (err) {
+                throw new Error(`no ${varName} environment variable`)
+            }
+        };
+        const variables = read_base64_json('PLATFORM_VARIABLES')
+        jwt_secret = variables["JWT_SECRET"]
+        jwt_expires_in = variables["JWT_EXPIRES_IN"]
+    }
+
     const token = req.headers.authorization.split(" ")[1];
-    verify(token, process.env.JWT_SECRET, {algorithm: "HS256"},  async (err, user) => {
+    verify(token, jwt_secret, { algorithm: "HS256" }, async (err, user) => {
         if (err) {
             res.status(401).end()
             return
@@ -19,7 +53,7 @@ exports.validateJWT = (req, res, next) => {
         try {
             req.user = await utiRepository.getUserByNomUti(user.name_user)
             return next()
-        } catch(e) {
+        } catch (e) {
             console.log(e)
             res.status(401).end()
         }
